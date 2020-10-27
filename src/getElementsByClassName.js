b83c95d95d7e5f3aa619e806024fe0e1137b410b
Jackson Pham <jackson.pham@gmail.com>
2020-10-26 20:17:31 -0700

    "Completed GetElementsByClassName"

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,6 +4,48 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className) {
-  // your code here
+var getElementsByClassName = function(className, node = document) {
+
+  //input: className
+  //output: array
+  //constraints:
+  //edgecase: must use recursion
+
+
+  //base case - when the function can produce a result without recursing
+  //recursive case - function which calls on itself
+
+  var nodeArray = [];
+
+  if (node.classList && node.classList.contains(className)) {
+    nodeArray.push(node);
+  }
+
+  if (node.children) {
+    for (var i = 0; i < node.children.length; i++) {
+      nodeArray = nodeArray.concat(getElementsByClassName(className, node.children[i]));
+    }
+  }
+
+  return nodeArray;
+
 };
+
+/*
+Psuedoecode
+  Notes on function:
+  node = document in the argument
+    node - is an HTML element in a document or DOM tree structure representing the HTML of the website. Every HTML element is a node.
+    document - or Document Object Model represents your web page,
+
+  //create nodeArray to house our nodes
+
+  //if node has a class && the className is what we are looking for
+    //put the node inside our nodeArray
+
+  //if node.children exist
+    //loop over the node.children
+      //set the nodeArray = to combining the the additional node childens classes
+
+*/
+
